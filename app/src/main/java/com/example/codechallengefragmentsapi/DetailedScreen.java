f776211b2016e7c41d146b28f08106a7f12bcdef
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-07 23:44:57 -0500

    inital commit

---
+++ app/src/main/java/com/example/codechallengefragmentsapi/DetailedScreen.java
@@ -0,0 +1,13 @@
+package com.example.codechallengefragmentsapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailedScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_screen);
+    }
+}
