26d169b569f1da10d81758f16afe50c96d2da363
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-08 21:54:08 -0500

    Creates ContactsService Interface

--- app/src/main/java/com/example/codechallengefragmentsapi/Network/ContactsService.java
+++ app/src/main/java/com/example/codechallengefragmentsapi/Network/ContactsService.java
@@ -1,12 +1,18 @@
 package com.example.codechallengefragmentsapi.Network;
 
+import com.example.codechallengefragmentsapi.Model.UserNameResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ContactsService {
 
-//     final String END_POINT = "users";
-//
-//
-//    @GET(END_POINT)
+    String END_POINT = "users";
+
+
+    @GET(END_POINT)
+    Call<List<UserNameResponse>> getUserList();
 
 }
